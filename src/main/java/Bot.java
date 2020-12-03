f510e17dba9806769b278da1a58b2c6a818cbb97
timmygunja <t.urusov13@mail.ru>
2020-12-03 18:33:12 +0300

    Bot initial

---
+++ src/main/java/Bot.java
@@ -0,0 +1,126 @@
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardRow;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+
+public class Bot extends TelegramLongPollingBot {
+
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot(new Bot());
+        } catch (TelegramApiRequestException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public synchronized void setButtons(SendMessage sendMessage) {
+
+// Создаем клавиуатуру
+
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+
+
+// Создаем список строк клавиатуры
+
+        List<KeyboardRow> keyboard = new ArrayList<>();
+
+
+// Первая строчка клавиатуры
+
+        KeyboardRow keyboardFirstRow = new KeyboardRow();
+
+// Добавляем кнопки в первую строчку клавиатуры
+
+        keyboardFirstRow.add(new KeyboardButton("Hello"));
+
+
+// Вторая строчка клавиатуры
+
+        KeyboardRow keyboardSecondRow = new KeyboardRow();
+
+// Добавляем кнопки во вторую строчку клавиатуры
+
+        keyboardSecondRow.add(new KeyboardButton("Help"));
+
+
+// Добавляем все строчки клавиатуры в список
+
+        keyboard.add(keyboardFirstRow);
+        keyboard.add(keyboardSecondRow);
+
+// и устанваливаем этот список нашей клавиатуре
+
+        replyKeyboardMarkup.setKeyboard(keyboard);
+    }
+
+
+    /**
+     * Метод для приема сообщений.
+     * @param update Содержит сообщение от пользователя.
+     */
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        String message = update.getMessage().getText();
+        sendMsg(update.getMessage().getChatId().toString(), message);
+    }
+
+
+    /**
+     * Метод для настройки сообщения и его отправки.
+     * @param chatId id чата
+     * @param s Строка, которую необходимо отправить в качестве сообщения.
+     */
+
+    public synchronized void sendMsg(String chatId, String s) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(s);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+//            log.log(Level.SEVERE, "Exception: ", e.toString());
+            System.out.println("Exception: " + e.toString());
+        }
+    }
+
+
+    /**
+     * Метод возвращает имя бота, указанное при регистрации.
+     * @return имя бота
+     */
+
+    @Override
+    public String getBotUsername() {
+        return "WeatherTelegramFinBot";
+    }
+
+
+    /**
+     * Метод возвращает token бота для связи с сервером Telegram
+     * @return token для бота
+     */
+
+    @Override
+    public String getBotToken() {
+        return "1425042061:AAGaKa5JvK3pHhh3x4PTtlpiAPyIe0kUSf8";
+    }
+}
